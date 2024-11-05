1e31ed6b30aa1215b8a5277405c11ae17950ae79
Ali <105427281+Alilock@users.noreply.github.com>
2024-11-05 20:56:31 +0400

    Add Firebase configuration and user management functionality

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyDVvmvb4AWYBYc0saIVWrBShNPwi7LJokc',
+    authDomain: 'fir-app-545ef.firebaseapp.com',
+    projectId: 'fir-app-545ef',
+    databaseURL: 'https://fir-app-545ef-default-rtdb.europe-west1.firebasedatabase.app/',
+    storageBucket: "fir-app-545ef.firebasestorage.app",
+    messagingSenderId: "455495746649",
+    appId: "1:455495746649:web:4b18d1b2a5ec5105565d48",
+    measurementId: "G-15XZ9EPRNL"
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+export const analytics = getAnalytics(app);
+export const db = getDatabase(app);
+
+export default app;
